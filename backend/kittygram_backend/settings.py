f868c9ea1fefd07ff02a4dafa8400a5632f0a611
Vladimir Tolstik <vltolstik@yandex.ru>
2023-10-30 01:51:55 +0300

    rezult

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,17 @@
-# flake8: noqa
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST',
+                          default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -53,31 +56,34 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.getenv('POSTGRES_DB', 'django'),
+      'USER': os.getenv('POSTGRES_USER', 'django'),
+      'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+      'HOST': os.getenv('DB_HOST', 'db'),
+      'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.UserAttributeSimilarityValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.MinimumLengthValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.CommonPasswordValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.NumericPasswordValidator',
+        },
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -90,8 +96,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
